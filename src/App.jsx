ebca55859da3fa3da1a40ce1ebcf044392ad7027
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-08-19 16:21:15 +0100

    styled the docker and imported icons from react icons for the docker icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { FaDiscord, FaFacebook, FaTwitter } from "react-icons/fa";
+import { IconContext } from "react-icons";
+
 function App() {
   return (
     <div className="app">
@@ -12,6 +15,7 @@ function Header() {
     <header className="header">
       <div className="logo">
         <img src="/public/vite.svg" alt="logo" />
+        <span>the43rd</span>
       </div>
 
       <div className="contact">
@@ -24,8 +28,48 @@ function Header() {
 function Docker() {
   return (
     <div className="docker">
-      <ul></ul>
+      <ul className="docker-list">
+        <DockItem>
+          <FaTwitter />
+        </DockItem>
+        <DockItem>
+          <FaFacebook />
+        </DockItem>
+        <DockItem>
+          <FaDiscord />
+        </DockItem>
+        <DockItem>
+          <FaTwitter />
+        </DockItem>
+        <DockItem>
+          <FaFacebook />
+        </DockItem>
+        <DockItem>
+          <FaDiscord />
+        </DockItem>
+      </ul>
     </div>
+  );
+}
+
+function DockItem({ children }) {
+  return (
+    <li className="dock-item">
+      <IconContext.Provider
+        value={{
+          style: {
+            fontSize: "35px",
+            color: "#000",
+            padding: "10px",
+            borderRadius: "50%",
+            display: "block",
+            background: "grey",
+          },
+        }}
+      >
+        <div>{children}</div>
+      </IconContext.Provider>
+    </li>
   );
 }
 
