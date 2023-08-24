f5cf86a192636bc42c8af4a10db889f7f003e955
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-08-24 16:00:35 +0100

    removed the docker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,72 @@
-import { FaDiscord, FaFacebook, FaTwitter } from "react-icons/fa";
-import { IconContext } from "react-icons";
-
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Docker />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Header />
+		</div>
+	);
 }
 
 function Header() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="/public/vite.svg" alt="logo" />
-        <span>the43rd</span>
-      </div>
+	return (
+		<header className="header">
+			<div className="logo">
+				<img src="/public/vite.svg" alt="logo" />
+				<span>the43rd</span>
+			</div>
 
-      <div className="contact">
-        <p>contact</p>
-      </div>
-    </header>
-  );
-}
-
-function Docker() {
-  return (
-    <div className="docker">
-      <ul className="docker-list">
-        <DockItem>
-          <FaTwitter />
-        </DockItem>
-        <DockItem>
-          <FaFacebook />
-        </DockItem>
-        <DockItem>
-          <FaDiscord />
-        </DockItem>
-        <DockItem>
-          <FaTwitter />
-        </DockItem>
-        <DockItem>
-          <FaFacebook />
-        </DockItem>
-        <DockItem>
-          <FaDiscord />
-        </DockItem>
-      </ul>
-    </div>
-  );
-}
-
-function DockItem({ children }) {
-  return (
-    <li className="dock-item">
-      <IconContext.Provider
-        value={{
-          style: {
-            fontSize: "35px",
-            color: "#000",
-            padding: "10px",
-            borderRadius: "50%",
-            display: "block",
-            background: "grey",
-          },
-        }}
-      >
-        <div>{children}</div>
-      </IconContext.Provider>
-    </li>
-  );
+			<div className="contact">
+				<p>contact</p>
+			</div>
+		</header>
+	);
 }
 
 export default App;
+
+// function Docker() {
+//   return (
+//     <div className="docker">
+//       <ul className="docker-list">
+//         <DockItem>
+//           <FaTwitter />
+//         </DockItem>
+//         <DockItem>
+//           <FaFacebook />
+//         </DockItem>
+//         <DockItem>
+//           <FaDiscord />
+//         </DockItem>
+//         <DockItem>
+//           <FaTwitter />
+//         </DockItem>
+//         <DockItem>
+//           <FaFacebook />
+//         </DockItem>
+//         <DockItem>
+//           <FaDiscord />
+//         </DockItem>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// function DockItem({ children }) {
+//   return (
+//     <li className="dock-item">
+//       <IconContext.Provider
+//         value={{
+//           style: {
+//             fontSize: "35px",
+//             color: "#000",
+//             padding: "10px",
+//             borderRadius: "50%",
+//             display: "block",
+//             background: "grey",
+//           },
+//         }}
+//       >
+//         <div>{children}</div>
+//       </IconContext.Provider>
+//     </li>
+//   );
+// }
