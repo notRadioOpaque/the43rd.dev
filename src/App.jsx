1050c280f8ca99a4146b7b7111bfb7842bd47c9c
the43rdsensei <ismailmuyideen.c05@gmail.com>
2023-09-04 09:53:44 +0100

    reorganized the about component UI, added new icons for the skill set component, made some other changes to the skill set component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,76 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useEffect, useState } from "react";
-import reactIcon from "../src/assets/react.svg";
 
 const skillArr = [
-	"Bootstrap",
-	"CSS",
-	"HTML",
-	"Javascript",
-	"React",
-	"SASS",
-	"Tailwind",
-	"Typescript",
+	{
+		title: "Bootstrap",
+		logo: "src/assets/bootstrap-logo.png",
+		bgColor: "rgba(103,58,183, .1)",
+	},
+	{
+		title: "CSS",
+		logo: "src/assets/css-logo.png",
+		bgColor: "rgba(114,175,220, .1)",
+	},
+	{
+		title: "HTML",
+		logo: "src/assets/html-logo.png",
+		bgColor: "rgba(236,91,54, .1)",
+	},
+	{
+		title: "Javascript",
+		logo: "src/assets/javascript.png",
+		bgColor: "rgba(251,215,74, .1)",
+	},
+	{
+		title: "React",
+		logo: "src/assets/react.svg",
+		bgColor: "rgba(90,216,249, .1)",
+	},
+	{
+		title: "SASS",
+		logo: "src/assets/sass.png",
+		bgColor: "rgba(228,131,174, .1)",
+	},
+	{
+		title: "Tailwind",
+		logo: "src/assets/tailwindcss.png",
+		bgColor: "rgba(71,172,194, .1)",
+	},
+	{
+		title: "Typescript",
+		logo: "src/assets/typescript.png",
+		bgColor: "rgba(32,118,210, .1)",
+	},
 ];
 
-const toolsArr = ["Git", "Github", "Figma", "NPM", "VsCode"];
+const toolsArr = [
+	{
+		title: "Figma",
+		logo: "src/assets/figma.png",
+		bgColor: "rgba(230,75,50, .1)",
+	},
+	{
+		title: "Git",
+		logo: "src/assets/git-logo.png",
+		bgColor: "rgba(238,81,53, .1)",
+	},
+	{
+		title: "Github",
+		logo: "src/assets/github.png",
+		bgColor: "rgba(56,56,56, .1)",
+	},
+	{
+		title: "NPM",
+		logo: "src/assets/npm-logo.png",
+		bgColor: "rgba(rgb(194,44,40, .1)",
+	},
+	{
+		title: "VS Code",
+		logo: "src/assets/visual-studio-code.png",
+		bgColor: "rgba(46,136,209, .1)",
+	},
+];
 
 function App() {
 	return (
@@ -81,18 +139,24 @@ function About() {
 	return (
 		<div className="about">
 			<div className="text">
-				<h3>
-					Ismael <span className="footer-link">Muyideen</span>,
-				</h3>
-				<h4>Front-end Web Developer</h4>
+				<h4>Hi, I'm Ismael Muyideen,</h4>
+				<h3>Building digital products, brands, and experience.</h3>
 				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta,
-					dicta dolorum vitae illo reprehenderit eum.
+					A Frontend Developer and Visual Designer with experience in web
+					design, brand identity and product design.
 				</p>
 			</div>
+
 			<div className="profile-pic">
 				<img src="src/assets/my-memoji.jpeg" alt="profile-pic" />
 			</div>
+
+			<button className="about-btn">
+				Connect with me{" "}
+				<span>
+					<img src="src/assets/react.svg" alt="" />
+				</span>
+			</button>
 		</div>
 	);
 }
@@ -100,16 +164,26 @@ function About() {
 function SkillSet() {
 	return (
 		<div className="skill-set">
-			<h2 className="skill-set-heading">Technology</h2>
+			<h2 className="skill-set-heading">Skill Set</h2>
 			<SkillGroup skillTitle={"Front-end"}>
 				{Array.from({ length: 8 }, (_, i) => (
-					<SkillItem key={i} skillItem={skillArr.at(i)} />
+					<SkillItem
+						key={i}
+						skillItem={skillArr.at(i).title}
+						skillLogo={skillArr.at(i).logo}
+						bgColor={skillArr.at(i).bgColor}
+					/>
 				))}
 			</SkillGroup>
 
 			<SkillGroup skillTitle={"Tools"}>
 				{Array.from({ length: 5 }, (_, i) => (
-					<SkillItem key={i} skillItem={toolsArr.at(i)} />
+					<SkillItem
+						key={i}
+						skillItem={toolsArr.at(i).title}
+						skillLogo={toolsArr.at(i).logo}
+						bgColor={toolsArr.at(i).bgColor}
+					/>
 				))}
 			</SkillGroup>
 		</div>
@@ -125,11 +199,11 @@ function SkillGroup({ skillTitle, children }) {
 	);
 }
 
-function SkillItem({ skillItem }) {
+function SkillItem({ skillItem, skillLogo, bgColor }) {
 	return (
 		<div className="skill-item">
-			<span>
-				<img src={reactIcon} alt="" />
+			<span style={{ background: `${bgColor}` }}>
+				<img src={skillLogo} alt="" />
 			</span>
 			<span>{skillItem}</span>
 		</div>
